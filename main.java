8ca4c6ed97c6b888e04b12f7eb3a829a53f8d184
HamiltonHamm <hammhakeem@gmail.com>
2025-12-05 01:56:11 -0600

    Update main.java

--- main.java
+++ main.java
@@ -1,5 +1,33 @@
  class main1{
  public static void main(String[] args) {
         // Program logic goes here
+  Scanner input = new Scanner(System.in);
+
+  System.out.println("===== Wizard Watch Input Center =====");
+
+  // asking the user for their location
+  System.out.println("Enter your current location (latitude,longitude): ");
+  String userLocationInput = input.nectLine();
+
+ System.out.println("You entered: " + userLocationInput);
+  System.out.print("Is this correct? (yes/no): ");
+  String comfirm = input.nextLine();
+
+  if (!confirm.equalsIgnoreCase("yes")) {
+            System.out.println("Please restart the program and enter correct location.");
+            return;
+        }
+
+  // ask user how far to search
+ System.out.print("Enter the maximum distance (in miles) to search for offenders: ");
+        double maxDistance = input.nextDouble();
+
+  System.out.println("\nSearching within " + maxDistance + " miles...");
+
+  // calls that will be implemented by  teammates ---
+  // Oneal will handle  storing user location
+
+  
+  
     }
 }
